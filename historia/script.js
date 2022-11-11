5a3b9f7a97b3374add0615391dc6f81f63bd19e5
Gamenight2008 <113185172+Gamenight2008@users.noreply.github.com>
2022-11-11 11:12:21 +0100

    img added

--- historia/script.js
+++ historia/script.js
@@ -1,23 +1,20 @@
 const btn = document.getElementById('btn');
 const text = document.getElementById('text');
 const page_title = document.getElementById('sub_title');
+const img = document.getElementById('img_el');
 let count = 0;
 
 btn.addEventListener('click', () => {
     count++;
     if(count === 1) {
-        page_title.innerText = 'Grecja';
-        btn.innerText = 'Następny';
-        text.textContent = '';
-    }else if(count === 2) {
         page_title.innerText = 'Ateny';
         btn.innerText = 'Następny';
         text.textContent = '';
-    }else if(count === 3) {
+    }else if(count === 2) {
         text.textContent = '';
         page_title.innerText = 'Sparta';
         btn.innerText = 'Następny';
-    }else if(count === 4) {
+    }else if(count === 3) {
         text.textContent = '';
         page_title.innerText = 'Macedonia';
         btn.innerText = 'Następny';
